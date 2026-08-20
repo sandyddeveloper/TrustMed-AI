2b2ba01c025aadc8e3442e5e81cb6066be787fe1
sandyddeveloper <sandyddeveloper@gmail.com>
2026-08-20 21:42:50 +0530

    version 1

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,13 +1,8 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
   reactStrictMode: true,
   transpilePackages: ["react-plotly.js", "plotly.js-dist-min"],
-  turbopack: {
-    root: path.resolve(__dirname, ".."),
-  },
 };
 
 export default nextConfig;
